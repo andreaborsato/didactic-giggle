4cfd9f8a8668d417059391fcf5bf69935c448677
Andrea <86688517+andreaborsato@users.noreply.github.com>
2023-04-12 00:12:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
-const path = require('path');
-var express = require('express');
+const path = require("path");
+var express = require("express");
 var app = express();
-const http = require('http');
-const socketIO= require('socket.io');
+const http = require("http");
+const socketIO = require("socket.io");
 
-const publicPath = path.join(__dirname, './public');
+const publicPath = path.join(__dirname, "./public");
 const port = process.env.PORT || 3000;
 
-let server = http.createServer(app)
+let server = http.createServer(app);
 let io = socketIO(server);
+let socketUsers = {};
+var users = [];
 
 app.use(express.static(publicPath));
 
-server.listen(port, ()=>{
-    console.log(`Server is up on port ${port}`);
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
 
+io.on("connection", (socket) => {
+  
+  console.log(`New user: ${socket.id} just connected`);
+  ///////////////////////////////
+  socket.emit("userId", `${socket.id}`);
+  
+  socket.on("adduser", (user)=>{
+      users[user]= socket.user;
+    
+    
+  })
 
+  socket.on('mouseActivity', function(data){
+    console.log(data);
+    socket.broadcast.emit('allMouseActivity', {session_id: socket.id, coords: data})
+    
+  })
 
-
-io.on("connection", (socket)=>{
-    console.log(`New user: ${socket.id} just connected`);
-    ///////////////////////////////
-
-    //////////////////////////////
-    socket.on("disconnect", function() {
-        console.log(`user: ${socket.id} has just disconnected from server`);
-      })
+  //////////////////////////////
+  socket.on("disconnect", function (user) {
+  
+      delete users[user];
+      
+    socket.emit('update', user)
+   
+    
+    console.log(`user: ${socket.id} has just disconnected from server`);
+    
+  });
+  
+  socket.on('update', function(user){
+    users[user] = user
+  })
 });
-
-
