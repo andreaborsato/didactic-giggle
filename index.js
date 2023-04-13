1db0f17dd576f9fc72ca266f04fab5025486f0c8
Glitch (simple-webapp) <none>
2023-04-13 15:32:04 +0000

    📁🎊 Updated with Glitch

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-const path = require("path");
-var express = require("express");
-var app = express();
-const http = require("http");
-const socketIO = require("socket.io");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const http = createServer(app);
 
-const publicPath = path.join(__dirname, "./public");
-const port = process.env.PORT || 3000;
-
-let server = http.createServer(app);
-let io = socketIO(server);
-let socketUsers = {};
-var users = [];
-
-app.use(express.static(publicPath));
-
-server.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + `/public/index.html`);
 });
+
+const io = new Server(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS"],
+    credentials: true,
+    transports: ["websocket"],
+  },
+  allowEIO3: true,
+});
+
+let port = process.env.PORT || 3001;
+
+http.listen(port, () =>
+  console.log("RUNNING SERVER ON http://localhost:" + port)
+);
+
 
 io.on("connection", (socket) => {
   
@@ -24,31 +34,26 @@ io.on("connection", (socket) => {
   ///////////////////////////////
   socket.emit("userId", `${socket.id}`);
   
-  socket.on("adduser", (user)=>{
-      users[user]= socket.user;
-    
-    
-  })
+ 
 
-  socket.on('mouseActivity', function(data){
+  socket.on('move', function(data){
     console.log(data);
-    socket.broadcast.emit('allMouseActivity', {session_id: socket.id, coords: data})
+    console.log(data.id);
+    socket.broadcast.emit('moveUpdate', {
+      data: data
+      
+    })
+    
+    
     
   })
 
   //////////////////////////////
   socket.on("disconnect", function (user) {
   
-      delete users[user];
-      
-    socket.emit('update', user)
    
     
     console.log(`user: ${socket.id} has just disconnected from server`);
     
   });
-  
-  socket.on('update', function(user){
-    users[user] = user
-  })
-});
+})
